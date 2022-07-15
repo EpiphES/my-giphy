57bf2563c5340968fc7a76d245a5d78a7929df51
Ekaterina Epiphanova <epiph@yandex.ru>
2022-07-15 20:38:56 +0500

    fix: remove event listener for reset button

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,6 @@ export default class Form {
     this._handleFormSubmit = handleFormSubmit;
 
     this._inputList = this._form.querySelectorAll(".form__input");
-    // this._submitButton = this._form.querySelector(".form__button_type_search");
     this._resetButton = this._form.querySelector(".form__button_type_reset");
   }
 
@@ -24,8 +23,5 @@ export default class Form {
 
   setEventListeners() {
     this._form.addEventListener("submit", this._submitFormHandler);
-    this._resetButton.addEventListener("click", () => {
-      this._form.reset();
-    });
   }
 }
